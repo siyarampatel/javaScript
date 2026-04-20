2aca9fa3d44a9d239dcece7f6c1d06ffc43a6aff
siyarampatel <siyarampateljbp@gmail.com>
2026-04-20 13:06:18 +0530

    Initial commit

---
+++ AC-1/1.js
@@ -0,0 +1,30 @@
+console.log("siyaram");
+age = 24;
+name = "siyaram";
+console.log(name);
+console.log(age);
+x=null;
+y=undefined;
+console.log(x);
+console.log(y);
+
+// boolean
+isFollow=true;
+console.log(isFollow);
+// dynamic
+name=25
+console.log(name);
+
+// variable name rules
+name="ram";
+NAME="siya";
+console.log(name);
+console.log(NAME);
+// . Variable names are case sensitive; "a" & "A" is different.
+
+// . Only letters, digits, underscore(_) and $ is allowed. (not even space)
+
+// . Only a letter, underscore(_) or $ should be 1st character.
+
+// . Reserved words cannot be variable names.
+
